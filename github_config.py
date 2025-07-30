83b9cfc2f50049c096b5d7a263c3d3659e0565c9
kamran98arbaaz <40592419+kamran98arbaaz@users.noreply.github.com>
2025-07-31 04:20:03 +0530

    Update github_config.py

--- github_config.py
+++ github_config.py
@@ -1,7 +1,7 @@
 import os
 
 # github_config.py
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+GITHUB_TOKEN = os.getenv("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")
 REPO_OWNER = "kamran98arbaaz"
 REPO_NAME = "loopin-home"
 FILE_PATH = "updates.json"
